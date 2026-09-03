3acd2292b69a9c16206abd0d7c2de981e816540a
Rohit Singh Sengar <rsengar257@gmail.com>
2026-09-03 13:06:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
-
+	let form=document.getElementById('form1');
+	let data = new FormData(form);
+	let firstName=data.get('fname');
+	let lastName=data.get('lname');
+	firstName=firstName.trim();
+	lastName=lastName.trim();
+	alert(`${firstName} ${lastName}`);
 }
